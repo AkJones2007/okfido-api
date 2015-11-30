686ca4267501264ae67f9d1718069fd9e6525728
Alex <AkJones2007@gmail.com>
2015-11-30 18:26:23 -0500

    Simplifies logic for favorites controller.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,16 @@
 class FavoritesController < ApplicationController
 
-  skip_before_action :authenticate, only: :index
+  skip_before_action :authenticate
 
   # Show all favorites for the current user
   def index
-    favorites = current_user.dogs.all
+    favorites = Favorite.all
     render json: favorites
   end
 
   # Show one favorite for the current user
   def show
-    index = params[:id].to_i - 1
-
-    if index < 0
-      render json: favorite.errors, status: :unprocessable_entity
-    end
-
-    all_favorites = current_user.dogs.all
-    favorite = all_favorites[index]
+    favorite = Favorite.find(params[:id])
     render json: favorite
   end
 
@@ -34,9 +27,7 @@ class FavoritesController < ApplicationController
 
   # Delete a favorite for the current user
   def destroy
-    index = params[:id].to_i - 1
-    all_favorites = Favorite.all.select{ |item| item.user_id == current_user.id }
-    favorite = all_favorites[index]
+    favorite = Favorite.find(params[:id])
     favorite.destroy
   end
 
--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,3 +1,3 @@
 class FavoriteSerializer < ActiveModel::Serializer
-  attributes :dog
+  attributes :id, :dog_id, :user_id
 end
