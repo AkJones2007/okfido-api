c4bd77b31ec25c48ce78e5f9f2d3bb62681cf59a
Alex <AkJones2007@gmail.com>
2015-12-11 15:41:48 -0500

    Tryinbg 'require: rack/cors'

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
+require "rack/cors"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
