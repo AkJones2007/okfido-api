712941898677ea08d173ddd42f732722369c2860
Alex <AkJones2007@gmail.com>
2015-12-11 13:53:15 -0500

    CORS issue still present. Trying again by removing protocol.

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Project2Api
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins 'https://AkJones2007.github.io/'
+        origins 'AkJones2007.github.io/'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
